178ce258091879d2f966279880c202f744c66a96
MiguellPC <miguelp326@gmail.com>
2022-07-22 15:16:40 -0300

    Solution without interface

---
+++ interface/interface1/Entities/CarRental.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interface1.Entities
+{
+    internal class CarRental
+    {
+        public DateTime Start { get; private set; }
+        public DateTime Finish { get; private set; }
+        public Vehicle Vehicle { get; private set; }
+        public Invoice Invoice { get; set; }
+
+        public CarRental(DateTime start, DateTime finish, Vehicle vehicle)
+        {
+            Start = start;
+            Finish = finish;
+            Vehicle = vehicle;
+        }
+    }
+}
