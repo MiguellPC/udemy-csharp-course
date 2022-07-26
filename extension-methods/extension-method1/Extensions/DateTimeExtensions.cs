4636903b426c7adf063e8e1aaf12805080a948a6
MiguellPC <miguelp326@gmail.com>
2022-07-26 15:02:59 -0300

    extension-methods

---
+++ extension-methods/extension-method1/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace System
+{
+    internal static class DateTimeExtensions
+    {
+        public static string TimeElapsed(this DateTime thisObj)
+        {
+            TimeSpan duration = DateTime.Now.Subtract(thisObj);
+
+            if (duration.TotalSeconds < 60.0)
+            {
+                return duration.Seconds + " seconds";
+            }
+            else if (duration.TotalMinutes < 60.0)
+            {
+                return duration.Minutes + " minutes " + duration.Seconds + " seconds";
+            }
+            else if (duration.TotalHours < 24.0)
+            {
+                return duration.Hours + " hours " + duration.Minutes + " minutes " + duration.Seconds + " seconds";
+            }
+            else
+            {
+                return duration.Days + " days " + duration.Hours + " hours " + duration.Minutes + " minutes " + duration.Seconds + " seconds";
+            }
+        }
+    }
+}
---
+++ extension-methods/extension-method1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace extension_method1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // DateTimeExceptions
+            DateTime dt = new DateTime(2022, 07, 25, 14, 50, 32);
+            Console.WriteLine(dt.TimeElapsed());
+
+            // StringExceptions
+            string s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
+        }
+    }
+}
